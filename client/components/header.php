5fcd380829ae2adc5f50d3df5120173cdf10b5a5
AxelLeo129 <axelleo129dev@gmail.com>
2021-06-14 00:13:13 -0600

    Project structure & first configuration client hearder.

---
+++ client/components/header.php
@@ -0,0 +1,44 @@
+<!--- TOP -->
+
+<div class="container-fluid superior--bar" id="top">
+    <div class="container">
+        <div class="row">
+            <!-- Redes sociales -->
+            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+                <ul>
+                    <li>
+                        <a href="https://facebook.com" target="_blank">
+                            <i class="fa fa-facebook social--network white--facebook" aria-hidden="true"></i>
+                        </a> 
+                    </li>
+                    <li>
+                        <a href="https://youtube.com" target="_blank">
+                            <i class="fa fa-youtube social--network white--youtube" aria-hidden="true"></i>
+                        </a> 
+                    </li>
+                    <li>
+                        <a href="https://instagram.com" target="_blank">
+                            <i class="fa fa-instragram social--network white--instragram" aria-hidden="true"></i>
+                        </a> 
+                    </li>
+                    <li>
+                        <a href="https://tweeter.com" target="_blank">
+                            <i class="fa fa-tweeter social--network white--tweeter" aria-hidden="true"></i>
+                        </a> 
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 register">
+                <ul>
+                    <li>
+                        <a href="#ventana_login" data-toggle="modal">Ingresar</a> 
+                    </li>
+                    <li>|</li>
+                    <li>
+                        <a href="#modal_registro" data-toggle="modal">Crear una cuenta</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ client/views/template.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimun-scale=1.0, maximun-scale=1.0, user-scalable=no">
+    <meta name="title" content="Libreria Neko">
+    <meta name="description" content="...">
+    <meta name="keyword" content="....">
+
+    <title>Libreria Neko</title>
+
+    <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+
+    <script src="js/plugins/jquery.min.js"></script>
+    <script src="js/plugins/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<?php
+
+    /* Header */    
+    include "components/header.php";
+
+
+?>
+
+
+
+
+</body>
+</html>
